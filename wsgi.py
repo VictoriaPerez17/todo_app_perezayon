fe4bc167afcdbca797a84816e59a85451075b16f
Victoria <ricardoperezayon9@gmail.com>
2024-12-10 23:13:23 -0600

    Fixed problems with WSGI

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,9 @@
 from app.src.main import app
 from app.src.db_create import init_db
 
+init_db()
+app.secret_key = "DeployProject?2024"
+app.config['SESSION_TYPE'] = 'filesystem'
+
 if __name__ == "__main__":
-    init_db()
-    app.secret_key = "DeployProject?2024"
     app.run()
